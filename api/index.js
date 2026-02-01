d6e94805f28477c749c4c83f82f2b0823fb3eddb
Salman <kingmansalman@gmail.com>
2026-02-01 13:29:21 +0700

    fix vercel serverless setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-
 import connectDb from "../configs/db.js";
 import userRouter from "../routes/userRoutes.js";
 import ownerRouter from "../routes/ownerRoutes.js";
@@ -9,20 +8,21 @@ import bookingRouter from "../routes/bookingRoutes.js";
 
 const app = express();
 
-// connect database
-await connectDb();
-
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// routes
-app.get("/", (req, res) => {
-  res.json({ success: true, message: "API running on Vercel 🚀" });
-});
+// connect database
+await connectDb();
 
+// routes
+app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/user", userRouter);
 app.use("/api/owner", ownerRouter);
 app.use("/api/bookings", bookingRouter);
 
+// ⚠️ uploads di vercel sebenernya ga persistent, tapi aman dulu
+app.use("/uploads", express.static("uploads"));
+
+// ⛔ JANGAN ADA app.listen
 export default app;
